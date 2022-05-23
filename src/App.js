4da8686f5c29e583a6ff3573c038f50e78b434e8
MinBaeBae <msbae92@gmail.com>
2022-05-23 16:29:23 +0900

    Coin Tracker API (React Practice)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(true);
+	const [coins, setCoins] = useState([]);
+
+	useEffect(() => {
+		fetch("https://api.coinpaprika.com/v1/tickers?limit=25")
+			.then((response) => response.json())
+			.then((json) => {
+				setCoins(json);
+				setLoading(false);
+			});
+	}, []);
+	return (
+		<div>
+			<h1>The Coins</h1>
+			{loading ? <strong>Loading...</strong> : null}
+			<ul>
+				{coins.map((coin) => (
+					<li>
+						{coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
+
+//         TO DO
+// 	const [toDo, setToDo] = useState("");
+// 	const [toDos, setToDos] = useState([]);
+// 	const onChange = (event) => setToDo(event.target.value);
+// 	const onSubmit = (event) => {
+// 		event.preventDefault();
+// 		if (toDo === "") {
+// 			return;
+// 		}
+// 		setToDos((currentArray) => [toDo, ...currentArray]);
+// 		setToDo("");
+// 	};
+
+// 	return (
+// 		<div>
+// 			<h1>My To Dos ({toDos.length})</h1>
+// 			<form onSubmit={onSubmit}>
+// 				<input
+// 					onChange={onChange}
+// 					value={toDo}
+// 					type="text"
+// 					placeholder="Write your to do..."
+// 				/>
+// 				<button>Add To Do</button>
+// 			</form>
+// 			<hr />
+// 			<ul>
+// 				{toDos.map((item, index) => (
+// 					<li key={index}>{item}</li>
+// 				))}
+// 			</ul>
+// 		</div>
+// 	);
